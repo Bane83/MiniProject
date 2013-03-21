6dffcfbf96a4731716c19129a212ca1c61d3cef4
Bane83 <banezemun83@gmail.com>
2013-03-21 13:46:16 +0100

    Update Korisnici.php
    Treba mi pomoc oko samog koda za pisanje bilo kakvog query-a!

--- MiniProject/application/models/DbTable/Korisnici.php
+++ MiniProject/application/models/DbTable/Korisnici.php
@@ -7,7 +7,9 @@ class Application_Model_DbTable_Korisnici extends Zend_Db_Table_Abstract
     
     
     public function proba($id){
-        
+        //ovo bi trebalo da bude samo primer neke funkcije kojoj kad prosledis ID iz tabele korisnici treba da dohvati 
+        //sve iz te tabele. Naravno to nece da radi, i trenutno ludim zbog toga. Ne znam kako da uopste uradim neki query 
+        //kad vec imam parametre unete u application.ini
         $db =  Zend_Db_Adapter_Abstract::getDefaultAdapter();
         
         $upit = "SELECT * FROM korisnici WHERE id=".$id;
